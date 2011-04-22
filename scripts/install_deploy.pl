82113006241442292b09ad9e99cc840bc7ebf8e1
David Z. Chen <david.z.chen@yale.edu>
2011-04-22 05:34:47 +0000

    Preliminary commit of completed Confp Perl module and install_deploy script
    git-svn-id: svn+ssh://svn.gersteinlab.org/repos/bioinfo/fusionSeq@581 2fd4de08-f6cc-4d2b-b124-baeea9ba92f3

--- scripts/install_deploy.pl
+++ scripts/install_deploy.pl
@@ -3,15 +3,21 @@
 use strict;
 use warnings;
 
+use FindBin;
+
+use lib "$FindBin::Bin";
+
 use Confp;
 use Cwd;
+
+use constant CGIS => "geneFusions_cgi showDetails_cgi seqViz_cgi findFusionPartner_cgi";
 
 my $path = getcwd() . "/../default.fusionseqrc";
 my $config = Confp->new($path);
 
 die("Cannot open default.fusionseqrc") if !$config;
 
-my $cgis = qw(geneFusions_cgi showDetails_cgi seqViz_cgi findFusionPartner_cgi);
-my $cmd = "mv " . $cgis . " " . $config->get('WEB_DATA_DIR');
+my $cmd = "mv " . CGIS . " " . $config->get('WEB_DATA_DIR');
 
+#print $cmd, "\n";
 system($cmd);
