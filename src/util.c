c4af650dbdd4be537981b771d1c0fa6aa8a0107f
Andrea Sboner <andrea.sboner.w@gmail.com>
2013-09-12 15:50:15 -0400

    Update: added writeFasta and updateStats
    writeFasta generates a reads.fa file with all the inter-transcripts
    reads. Used by gfrRibosomalFilter, gfrMitochondrialFilter and
    gfrSmallScaleHomologyFilter
    updateStats, used by the same programs, updates the numInter counts
    after removing the reads

--- src/util.c
+++ src/util.c
@@ -4,6 +4,7 @@
 #include <bios/bits.h>
 
 #include "util.h"
+#include "gfr.h"
 
 int getNucleotideOverlap ( BlatQuery* blQ ) {
   int l;
@@ -29,6 +30,73 @@ int getNucleotideOverlap ( BlatQuery* blQ ) {
   //int overlap = 50; //bitCountRange( bitOverlap, 0, readSize);
   //bitFree( &bitOverlap);
   return maxOverlap;
+}
+
+
+
+void updateStats( GfrEntry* currGE ) {
+  int i;
+  float numInters=0.0;
+  for( i = 0; i < arrayMax( currGE->interReads ); i++ ) {
+    GfrInterRead* currGIR = arrp( currGE->interReads, i, GfrInterRead );
+    if( currGIR->flag ) continue;
+    char* read1 = arru( currGE->readsTranscript1, i, char* );
+    char* read2 = arru( currGE->readsTranscript2, i, char* );
+    if( read1==NULL || read2==NULL || (currGIR->readEnd1 - currGIR->readStart1 + 1) == 0 | 
+	(currGIR->readEnd2 - currGIR->readStart2 + 1) == 0 ) 
+      die("Something is wrong with the inter pairs: read1[%s](e%d-s%d +1 )=%d read2[%s](e%d-s%d + 1 )=%d", 
+	  read1, currGIR->readEnd1, currGIR->readStart1, (currGIR->readEnd1 - currGIR->readStart1 + 1),
+	  read2, currGIR->readEnd2, currGIR->readStart2, (currGIR->readEnd2 - currGIR->readStart2 + 1) );
+    if( (currGIR->readEnd1 - currGIR->readStart1 + 1) != strlen(read1) &
+	(currGIR->readEnd2 - currGIR->readStart2 + 1) != strlen(read2) ) {
+      numInters += 0.25;
+    } else if ( (currGIR->readEnd1 - currGIR->readStart1 + 1) != strlen(read1) |
+		(currGIR->readEnd2 - currGIR->readStart2 + 1) != strlen(read2) ) {
+      numInters += 0.5;
+    } else {
+      numInters += 1.0;
+    }
+  }
+  currGE->numInter = numInters;
+}
+
+
+void writeFasta( GfrEntry* currGE, unsigned int *minReadSize, char* directory ) 
+{
+  FILE *freads;
+  int l, readSize1, readSize2;
+  // creating one fasta files with the reads
+  Stringa readsFA = stringCreate( 100 ); 
+  
+  // creating the fasta files with the reads 
+  stringPrintf( readsFA, "%s/%s_reads.fa", directory, currGE->id);
+  freads = fopen ( string(readsFA) ,"w");
+  if (freads == NULL) {
+    die ("Unable to open file: %s",string (readsFA));
+  }     
+  if (arrayMax(currGE->readsTranscript1) != arrayMax(currGE->readsTranscript2))
+    die("Error: different number of inter-transcript reads %d vs. %d", 
+	arrayMax( currGE->readsTranscript1),
+	arrayMax( currGE->readsTranscript2) );
+  
+  // writing the reads into file
+  for (l = 0; l < arrayMax (currGE->readsTranscript1); l++) {      
+    char* currRead1 = hlr_strdup( textItem (currGE->readsTranscript1,l)); // read1
+    char* currRead2 = hlr_strdup( textItem (currGE->readsTranscript2,l)); // read2
+    fprintf( freads, ">%d/1\n%s\n>%d/2\n%s\n", l, currRead1, l, currRead2 );
+    readSize1 = strlen( currRead1 );
+    readSize2 = strlen( currRead2 );
+    
+    if(readSize1 != readSize2 )
+      die("The two reads have different lengths: 1:%d vs 2:%d", readSize1, readSize2);
+    if ( readSize1 < *minReadSize )
+      *minReadSize = readSize1 ;
+    hlr_free( currRead1 );
+    hlr_free( currRead2 );
+  }
+  fclose( freads ); 
+  freads=NULL;
+  stringDestroy( readsFA );
 }
 
 Array util_readKnownGeneXrefs (char* fileName)
